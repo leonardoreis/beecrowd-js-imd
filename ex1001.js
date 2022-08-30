56a39a3c3b1f94743f3076716f6bb53a6c7a2998
Leonardo Reis <leonardo.demian@gmail.com>
2022-08-29 23:30:47 -0300

    primeiro exercício do beecrowd - EX1001 - 2

---
+++ ex1001.js
@@ -0,0 +1,30 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+[A, B] = input.split("\n");
+
+var X = `X = ${(Number(A) + Number(B))}`;
+console.log(X);
+
+/*
+As linhas acima (4 à 7) resolveram com sucesso o BEECROWD.
+A partir de agora, será feita a versão WEB, usando FUNÇÃO.
+*/
+/*
+function somar(){
+    const a = Number(document.getElementById("A").value);
+    const b = Number(document.getElementById("B").value);
+    const x = (a + b);
+    alert(a);
+    alert(b);
+    alert(x);
+    document.getElementById("Resultado").innerHTML = `X = ${x}`;
+}
+*/
+
+function limpar(){
+    document.getElementById("A").value = "";
+    document.getElementById("B").value = "";
+    
+document.getElementById("Resultado").innerHTML = "";
+}
